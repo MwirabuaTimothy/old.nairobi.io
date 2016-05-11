84caabd951813681e22d4fd323e35a0bf5a9e28c
Derrick <omondiderrick4@gmail.com>
2016-05-11 16:36:24 +0300

    change description spelling

--- database/seeds/Access/ToursTableSeeder.php
+++ database/seeds/Access/ToursTableSeeder.php
@@ -13,10 +13,10 @@ class ToursTableSeeder extends Seeder {
 		DB::table('tours')->delete();
 
 		DB::table('tours')->insert([
-			['title' => 'First tour', 'description' => 'Some good trip', 'user_id' => 1, 'rate' => 10, 'accomodates' => 1, 'rules' => 'no smoking'],
-			['title' => 'Mara tour', 'description' => 'You will enjoy', 'user_id' => 2, 'rate' => 10, 'accomodates' => 2, 'rules' => 'no drinking'],
-			['title' => 'Gedi ruins', 'description' => 'Come join', 'user_id' => 1, 'rate' => 13, 'accomodates' => 3, 'rules' => 'no lateness'],
-			['title' => 'Magadi tour', 'description' => 'A lot of swimming', 'user_id' => 2, 'rate' => 15, 'accomodates' => 4, 'rules' => 'no outside food'],
+			['title' => 'First tour', 'description' => 'Some good trip', 'user_id' => 1, 'rate' => 10, 'accommodates' => 1, 'rules' => 'no smoking'],
+			['title' => 'Mara tour', 'description' => 'You will enjoy', 'user_id' => 2, 'rate' => 10, 'accommodates' => 2, 'rules' => 'no drinking'],
+			['title' => 'Gedi ruins', 'description' => 'Come join', 'user_id' => 1, 'rate' => 13, 'accommodates' => 3, 'rules' => 'no lateness'],
+			['title' => 'Magadi tour', 'description' => 'A lot of swimming', 'user_id' => 2, 'rate' => 15, 'accommodates' => 4, 'rules' => 'no outside food'],
 		]);
 	}
 }
