0ef21f28ecd6a20af453e3ac1a7ec3b083a5d4c9
Derrick <omondiderrick4@gmail.com>
2016-05-11 13:23:02 +0300

    create tours table migration

---
+++ database/migrations/2016_05_11_093652_create_tours_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateToursTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('tours', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->string('description');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->string('image');
+			$table->timestamp('available_from');
+			$table->timestamp('available_to');
+			$table->string('rate');
+			$table->string('accomodates');
+			$table->string('rules');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('tours');
+	}
+}
