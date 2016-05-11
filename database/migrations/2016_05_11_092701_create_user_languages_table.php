0e82605f5223ee227d9682cf81dabee73267fb12
Derrick <omondiderrick4@gmail.com>
2016-05-11 13:22:50 +0300

    create user languages pivot table migration

---
+++ database/migrations/2016_05_11_092701_create_user_languages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserLanguagesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('user_languages', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('language_id')->unsigned();
+			$table->foreign('language_id')->references('id')->on('languages');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('user_languages');
+	}
+}
