addf8fb8202c81eed120bee8027b3aa709253d9a
Derrick <omondiderrick4@gmail.com>
2016-05-11 13:22:18 +0300

    create languages table migration

---
+++ database/migrations/2016_05_11_065416_create_languages_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLanguagesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('languages', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('language_code');
+			$table->string('language_name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('languages');
+	}
+}
