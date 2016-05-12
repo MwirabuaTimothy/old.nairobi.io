027b75837dfae15e1476207b2e8e8a15d0be9879
Derrick <omondiderrick4@gmail.com>
2016-05-12 16:30:50 +0300

    routes for social login facebook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,28 @@
 <?php
 
-
 /**
  * Testing controller
  */
 $router->controller('tests', 'TestsController');
 
+Route::group(['middleware' => 'web'], function () {
+	Route::get('redirect', ['uses' => 'RegistrationController@redirect', 'as' => 'redirect']);
+	Route::get('callback', ['uses' => 'RegistrationController@callback', 'as' => 'callback']);
+	/**
+	 * Switch between the included languages
+	 */
+	Route::group(['namespace' => 'Language'], function () {
+		require (__DIR__ . '/Routes/Language/Language.php');
+	});
 
-Route::group(['middleware' => 'web'], function() {
-    /**
-     * Switch between the included languages
-     */
-    Route::group(['namespace' => 'Language'], function () {
-        require (__DIR__ . '/Routes/Language/Language.php');
-    });
-
-    /**
-     * Frontend Routes
-     * Namespaces indicate folder structure
-     */
-    Route::group(['namespace' => 'Frontend'], function () {
-        require (__DIR__ . '/Routes/Frontend/Frontend.php');
-        require (__DIR__ . '/Routes/Frontend/Access.php');
-    });
+	/**
+	 * Frontend Routes
+	 * Namespaces indicate folder structure
+	 */
+	Route::group(['namespace' => 'Frontend'], function () {
+		require (__DIR__ . '/Routes/Frontend/Frontend.php');
+		require (__DIR__ . '/Routes/Frontend/Access.php');
+	});
 });
 
 /**
@@ -31,14 +31,14 @@ Route::group(['middleware' => 'web'], function() {
  * Admin middleware groups web, auth, and routeNeedsPermission
  */
 Route::group(['namespace' => 'Backend', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
-    /**
-     * These routes need view-backend permission
-     * (good if you want to allow more than one group in the backend,
-     * then limit the backend features by different roles or permissions)
-     *
-     * Note: Administrator has all permissions so you do not have to specify the administrator role everywhere.
-     */
-    require (__DIR__ . '/Routes/Backend/Dashboard.php');
-    require (__DIR__ . '/Routes/Backend/Access.php');
-    require (__DIR__ . '/Routes/Backend/LogViewer.php');
+	/**
+	 * These routes need view-backend permission
+	 * (good if you want to allow more than one group in the backend,
+	 * then limit the backend features by different roles or permissions)
+	 *
+	 * Note: Administrator has all permissions so you do not have to specify the administrator role everywhere.
+	 */
+	require (__DIR__ . '/Routes/Backend/Dashboard.php');
+	require (__DIR__ . '/Routes/Backend/Access.php');
+	require (__DIR__ . '/Routes/Backend/LogViewer.php');
 });
