39dc0ce5cc63b3189f0c52d2740e6f2a0a0636ab
deryq <omondiderrick4@gmail.com>
2016-05-13 11:57:50 +0300

    get all needed user information from facebook api

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -11,22 +11,35 @@ class RegistrationController extends Controller {
 	 * @return [type] [description]
 	 */
 	public function redirect() {
-		return Socialite::driver('facebook')->redirect();
+		return Socialite::driver('facebook')->fields(['first_name', 'last_name', 'email', 'gender', 'birthday', 'about', 'bio', 'education', 'hometown', 'location', 'work'])->redirect();
 	}
 
 	public function callback() {
 		// when facebook call us a with token
-		$user = Socialite::driver('facebook')->user();
+		// GET /v2.6/{user-id} HTTP/1.1
+		$user = Socialite::driver('facebook')->fields(['first_name', 'last_name', 'email', 'gender', 'birthday', 'about', 'bio', 'education', 'hometown', 'location', 'work'])->user();
+		//dd($user);
+		$last_sch = $user->user['education'][count($user->user['education']) - 1];
+
+		return $last_sch['school']['name'];
+
 		$existing_user = User::where('email', $user->email)->first();
-		$separate_name = explode(" ", $user->name);
+
 		if ($existing_user === null) {
 
 			$existing_user = User::create([
-				'fb_uid' => $user->id,
-				'email' => $user->email,
-				'gender' => $user['gender'],
-				'first_name' => $separate_name[0],
-				'last_name' => $separate_name[1],
+				'first_name' => $user->user['first_name'],
+				'last_name' => $user->user['last_name'],
+				'email' => $user->user['email'],
+				'gender' => $user->user['gender'],
+				'fb_uid' => $user->user['id'],
+				'dob' => $user->user['birthday'],
+				'about' => $user->user['about'],
+				'bio' => $user->user['bio'],
+				'hometown' => $user->user['hometown']['name'],
+				'current_city' => $user->user['location']['name'],
+				'education_institution' => $last_sch['school']['name'],
+				'image' => $user->avatar,
 			]);
 		}
 		$existing_user->first_name = $separate_name[0];
