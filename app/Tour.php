056ac011ed614b7362a002f25a624697e374a200
Derrick <omondiderrick4@gmail.com>
2016-05-20 15:20:14 +0300

    add soft deletes to tour model and fillables for mass assignment

--- app/Tour.php
+++ app/Tour.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tour extends Model {
+	use SoftDeletes;
+	protected $dates = ['deleted_at'];
+	protected $fillable = ['title', 'description', 'available_from', 'available_to', 'image', 'rate', 'rules'];
 
 	/**
 	 * @return \Illuminate\Database\Eloquent\Relations\belongsTo
